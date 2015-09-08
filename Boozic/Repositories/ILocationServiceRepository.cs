fa9157231cd3596daf3c0889d9d85e5ff9d5e89d
Timmy Chandy <timmychandy@hotmail.com>
2015-09-08 13:56:55 -0400

    Repository

---
+++ Boozic/Repositories/ILocationServiceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boozic.Models;
+
+namespace Boozic.Repositories
+{
+   public interface ILocationServiceRepository
+    {
+        List<StoreInfo> getStores(double Latitude, double Longitude, double Radius);
+      
+      
+    }
+}
